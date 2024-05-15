285357cda72000a802d92a4312947c057fd20ee5
AlfredoCastrogiovanni <castrogiovanni.alfredo03@gmail.com>
2024-05-15 14:24:23 +0200

    MetaGala done

--- src/org/learning/MetGala.java
+++ src/org/learning/MetGala.java
@@ -1,5 +1,38 @@
 package org.learning;
 
-public class MetGala {
+import java.util.Scanner;
 
+public class MetGala {
+    public static void main(String[] args) {
+        boolean access = false;
+
+        String[] guestList = {
+            "Jennifer Lopez",
+            "Zendaya",
+            "Chris Hemsworth",
+            "Bad Bunny",
+            "Cara Delevingne",
+            "Kendall Jenner",
+            "Uma Thurman",
+            "Damiano David",
+            "Luca Guadagnino"
+        };
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String guestName = scanner.nextLine();
+
+        for (int i = 0; i < guestList.length; i++) {
+            if (guestList[i].equals(guestName)) {
+                access = true;
+            }
+        }
+
+        if (access) {
+            System.out.println("Access Allowed");
+        } else {
+            System.out.println("Access Denied");
+        }
+    }
 }
