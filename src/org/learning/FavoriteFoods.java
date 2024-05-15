d5d9dbf4a095ce6a7a5eb13144c658f31d333bcd
AlfredoCastrogiovanni <castrogiovanni.alfredo03@gmail.com>
2024-05-15 14:11:29 +0200

    FavoriteFoods done

--- src/org/learning/FavoriteFoods.java
+++ src/org/learning/FavoriteFoods.java
@@ -1,5 +1,12 @@
 package org.learning;
 
 public class FavoriteFoods {
+    public static void main(String[] args) {
+        String[] foods = {"Pizza", "Pasta", "Gelato", "Risotto", "Tiramisù"};
 
+        System.out.println("Ranking Lenght: " + foods.length);
+        System.out.println("First Position: " + foods[0]);
+        System.out.println("Last Position: " + foods[foods.length - 1]);
+        System.out.println("Middle Position: " + foods[foods.length / 2]);
+    }
 }
